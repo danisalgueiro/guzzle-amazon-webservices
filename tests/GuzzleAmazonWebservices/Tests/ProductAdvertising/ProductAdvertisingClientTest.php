88bd33fe2047a2d734ffda89bd33e1e66c3be17f
Dani Salgueiro <dani@danisalgueiro.com>
2012-08-02 15:24:53 +0200

    Change tests to Tests namespace

--- tests/GuzzleAmazonWebservices/Tests/ProductAdvertising/ProductAdvertisingClientTest.php
+++ tests/GuzzleAmazonWebservices/Tests/ProductAdvertising/ProductAdvertisingClientTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GuzzleAmazonWebservices\ProductAdvertising;
+namespace GuzzleAmazonWebservices\Tests\ProductAdvertising;
 
 use Guzzle\Tests\GuzzleTestCase;
 use GuzzleAmazonWebservices\ProductAdvertising\ProductAdvertisingClient;
@@ -31,5 +31,22 @@ class ProductAdvertisingClientTest extends GuzzleTestCase
         $client = ProductAdvertisingClient::factory(array(
             'secret_key' => '9876'
         ));
+    }
+    
+    public function testAll()
+    {
+        $client = ProductAdvertisingClient::factory(array(
+            'access_key' => 'AKIAJ75M4ZDNYIT33TBQ',
+            'secret_key' => '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',
+            'associate_tag' => ''
+        ));
+        
+        $command = $client->getCommand('ItemSearch');
+        $command->setTitle('harry potter');
+        $command->setSearchIndex('Books');
+        $command->setResponseGroup('Small');
+        
+        $result = $client->execute($command);
+        print_r($result);
     }
 }
